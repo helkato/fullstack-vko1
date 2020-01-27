4de24e9f065676ceb3ed532e7e15a8ecf49481f8
kpo <kati.ojala@helsinki.fi>
2020-01-27 11:48:18 +0200

    unicafe 1.7 tehty

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -6,7 +6,7 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
+    const all= (good +neutral+ bad)
 
     return (
         <div>
@@ -20,11 +20,16 @@ const App = () => {
             <button onClick={() => setBad(bad + 1)}>
                 bad
             </button>
-            <h1>Statistics</h1>
-            <div> {good}</div>
-            <div>{neutral}</div>
-            <div>{bad}</div>
 
+            <h1>Statistics</h1>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+
+
+            <div>all {all}</div>
+            <div> average {(good-bad)/all}</div>
+    <div> positive {good/all*100} %</div>
         </div >
     )
 }
